7389b121d6dbfa6864ef8aff95466c6fa00230b6
adamogamamkt <adamo.alighieri@distribuidoragama.com.br>
2024-08-19 15:59:22 -0300

    Sanavita

--- js/scripts.js
+++ js/scripts.js
@@ -599,3 +599,129 @@ document.addEventListener("DOMContentLoaded", function() {
 
   xhr.send();
 });
+
+// TABELA SANAVITA VITAMINAS
+document.addEventListener("DOMContentLoaded", function() {
+  // URL do arquivo txt
+  var txtFile = 'dados/dados_sanavita_vitaminas.txt';
+
+  // Cria um objeto XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', txtFile, true);
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Obtém o texto do arquivo
+      var txtData = xhr.responseText;
+
+      // Divide o texto em linhas (cada linha é separada por uma nova linha)
+      var lines = txtData.split('\n');
+      var tableBody = document.querySelector("#dados_sanavita_vitaminas tbody");
+
+      // Itera sobre cada linha
+      lines.forEach(function(line, index) {
+        // Divide a linha em colunas com base na tabulação
+        var columns = line.split('\t');
+
+        // Cria uma nova linha na tabela
+        var row = document.createElement('tr');
+
+        // Adiciona cada coluna à linha
+        columns.forEach(function(column) {
+          var cell = document.createElement('td');
+          cell.textContent = column;
+          row.appendChild(cell);
+        });
+        
+        // Adiciona a linha ao corpo da tabela
+        tableBody.appendChild(row);
+      });
+    }
+  };
+
+  xhr.send();
+});
+
+// TABELA SANAVITA VITAL
+document.addEventListener("DOMContentLoaded", function() {
+  // URL do arquivo txt
+  var txtFile = 'dados/dados_sanavita_vital.txt';
+
+  // Cria um objeto XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', txtFile, true);
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Obtém o texto do arquivo
+      var txtData = xhr.responseText;
+
+      // Divide o texto em linhas (cada linha é separada por uma nova linha)
+      var lines = txtData.split('\n');
+      var tableBody = document.querySelector("#dados_sanavita_vital tbody");
+
+      // Itera sobre cada linha
+      lines.forEach(function(line, index) {
+        // Divide a linha em colunas com base na tabulação
+        var columns = line.split('\t');
+
+        // Cria uma nova linha na tabela
+        var row = document.createElement('tr');
+
+        // Adiciona cada coluna à linha
+        columns.forEach(function(column) {
+          var cell = document.createElement('td');
+          cell.textContent = column;
+          row.appendChild(cell);
+        });
+        
+        // Adiciona a linha ao corpo da tabela
+        tableBody.appendChild(row);
+      });
+    }
+  };
+
+  xhr.send();
+});
+
+// TABELA SANAVITA ESSENCIAL
+document.addEventListener("DOMContentLoaded", function() {
+  // URL do arquivo txt
+  var txtFile = 'dados/dados_sanavita_essencial.txt';
+
+  // Cria um objeto XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', txtFile, true);
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Obtém o texto do arquivo
+      var txtData = xhr.responseText;
+
+      // Divide o texto em linhas (cada linha é separada por uma nova linha)
+      var lines = txtData.split('\n');
+      var tableBody = document.querySelector("#dados_sanavita_essencial tbody");
+
+      // Itera sobre cada linha
+      lines.forEach(function(line, index) {
+        // Divide a linha em colunas com base na tabulação
+        var columns = line.split('\t');
+
+        // Cria uma nova linha na tabela
+        var row = document.createElement('tr');
+
+        // Adiciona cada coluna à linha
+        columns.forEach(function(column) {
+          var cell = document.createElement('td');
+          cell.textContent = column;
+          row.appendChild(cell);
+        });
+        
+        // Adiciona a linha ao corpo da tabela
+        tableBody.appendChild(row);
+      });
+    }
+  };
+
+  xhr.send();
+});
