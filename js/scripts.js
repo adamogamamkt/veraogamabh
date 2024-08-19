06885a9319bb0e63f5850f69b70a8e297c8080cd
adamogamamkt <adamo.alighieri@distribuidoragama.com.br>
2024-08-19 12:04:08 -0300

    Integral Médica

--- js/scripts.js
+++ js/scripts.js
@@ -19,317 +19,6 @@ fetch('header.html')
     });
 });
 
-// Atualize o cache dos arquivos CSS e JS
-window.onload = function() {
-  // Função para adicionar um timestamp ao final da URL
-  function refreshCache(id) {
-      var element = document.getElementById(id);
-      if (element) {
-          var currentSrc = element.getAttribute('href') || element.getAttribute('src');
-          var newSrc = currentSrc.split('?')[0] + '?v=' + new Date().getTime();
-          if (element.tagName.toLowerCase() === 'link') {
-              element.setAttribute('href', newSrc);
-          } else if (element.tagName.toLowerCase() === 'script') {
-              element.setAttribute('src', newSrc);
-          }
-      }
-    }
-};
-
-// TABELA MOSKITOFF
-document.addEventListener("DOMContentLoaded", function() {
-    // URL do arquivo txt
-    var txtFile = 'dados/dados_moskitoff.txt';
-
-    // Cria um objeto XMLHttpRequest
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', txtFile, true);
-
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        // Obtém o texto do arquivo
-        var txtData = xhr.responseText;
-
-        // Divide o texto em linhas (cada linha é separada por uma nova linha)
-        var lines = txtData.split('\n');
-        var tableBody = document.querySelector("#dados_moskitoff tbody");
-
-        // Itera sobre cada linha
-        lines.forEach(function(line) {
-          // Divide a linha em colunas com base na tabulação
-          var columns = line.split('\t');
-
-          // Cria uma nova linha na tabela
-          var row = document.createElement('tr');
-
-          // Adiciona cada coluna à linha
-          columns.forEach(function(column) {
-            var cell = document.createElement('td');
-            cell.textContent = column;
-            row.appendChild(cell);
-          });
-
-          // Adiciona a linha ao corpo da tabela
-          tableBody.appendChild(row);
-        });
-      }
-    };
-
-    xhr.send();
-  });
-
-// TABELA SUNLESS
-document.addEventListener("DOMContentLoaded", function() {
-  // URL do arquivo txt
-  var txtFile = 'dados/dados_sunless.txt';
-
-  // Cria um objeto XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', txtFile, true);
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      // Obtém o texto do arquivo
-      var txtData = xhr.responseText;
-
-      // Divide o texto em linhas (cada linha é separada por uma nova linha)
-      var lines = txtData.split('\n');
-      var tableBody = document.querySelector("#dados_sunless tbody");
-
-      // Itera sobre cada linha
-      lines.forEach(function(line) {
-        // Divide a linha em colunas com base na tabulação
-        var columns = line.split('\t');
-
-        // Cria uma nova linha na tabela
-        var row = document.createElement('tr');
-
-        // Adiciona cada coluna à linha
-        columns.forEach(function(column) {
-          var cell = document.createElement('td');
-          cell.textContent = column;
-          row.appendChild(cell);
-        });
-
-        // Adiciona a linha ao corpo da tabela
-        tableBody.appendChild(row);
-      });
-    }
-  };
-
-  xhr.send();
-});
-
-// TABELA NEGRA ROSA HAIR
-document.addEventListener("DOMContentLoaded", function() {
-  // URL do arquivo txt
-  var txtFile = 'dados/dados_negrarosa_hair.txt';
-
-  // Cria um objeto XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', txtFile, true);
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      // Obtém o texto do arquivo
-      var txtData = xhr.responseText;
-
-      // Divide o texto em linhas (cada linha é separada por uma nova linha)
-      var lines = txtData.split('\n');
-      var tableBody = document.querySelector("#dados_negrarosa_hair tbody");
-
-      // Itera sobre cada linha
-      lines.forEach(function(line) {
-        // Divide a linha em colunas com base na tabulação
-        var columns = line.split('\t');
-
-        // Cria uma nova linha na tabela
-        var row = document.createElement('tr');
-
-        // Adiciona cada coluna à linha
-        columns.forEach(function(column) {
-          var cell = document.createElement('td');
-          cell.textContent = column;
-          row.appendChild(cell);
-        });
-
-        // Adiciona a linha ao corpo da tabela
-        tableBody.appendChild(row);
-      });
-    }
-  };
-
-  xhr.send();
-});
-
-// TABELA NEGRA ROSA SKIN
-document.addEventListener("DOMContentLoaded", function() {
-  // URL do arquivo txt
-  var txtFile = 'dados/dados_negrarosa_skin.txt';
-
-  // Cria um objeto XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', txtFile, true);
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      // Obtém o texto do arquivo
-      var txtData = xhr.responseText;
-
-      // Divide o texto em linhas (cada linha é separada por uma nova linha)
-      var lines = txtData.split('\n');
-      var tableBody = document.querySelector("#dados_negrarosa_skin tbody");
-
-      // Itera sobre cada linha
-      lines.forEach(function(line) {
-        // Divide a linha em colunas com base na tabulação
-        var columns = line.split('\t');
-
-        // Cria uma nova linha na tabela
-        var row = document.createElement('tr');
-
-        // Adiciona cada coluna à linha
-        columns.forEach(function(column) {
-          var cell = document.createElement('td');
-          cell.textContent = column;
-          row.appendChild(cell);
-        });
-
-        // Adiciona a linha ao corpo da tabela
-        tableBody.appendChild(row);
-      });
-    }
-  };
-
-  xhr.send();
-});
-
-// TABELA SANAVITA VITAMINAS
-document.addEventListener("DOMContentLoaded", function() {
-  // URL do arquivo txt
-  var txtFile = 'dados/dados_sanavita_vitaminas.txt';
-
-  // Cria um objeto XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', txtFile, true);
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      // Obtém o texto do arquivo
-      var txtData = xhr.responseText;
-
-      // Divide o texto em linhas (cada linha é separada por uma nova linha)
-      var lines = txtData.split('\n');
-      var tableBody = document.querySelector("#dados_sanavita_vitaminas tbody");
-
-      // Itera sobre cada linha
-      lines.forEach(function(line) {
-        // Divide a linha em colunas com base na tabulação
-        var columns = line.split('\t');
-
-        // Cria uma nova linha na tabela
-        var row = document.createElement('tr');
-
-        // Adiciona cada coluna à linha
-        columns.forEach(function(column) {
-          var cell = document.createElement('td');
-          cell.textContent = column;
-          row.appendChild(cell);
-        });
-
-        // Adiciona a linha ao corpo da tabela
-        tableBody.appendChild(row);
-      });
-    }
-  };
-
-  xhr.send();
-});
-
-// TABELA SANAVITA ESSENCIAL
-document.addEventListener("DOMContentLoaded", function() {
-  // URL do arquivo txt
-  var txtFile = 'dados/dados_sanavita_essencial.txt';
-
-  // Cria um objeto XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', txtFile, true);
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      // Obtém o texto do arquivo
-      var txtData = xhr.responseText;
-
-      // Divide o texto em linhas (cada linha é separada por uma nova linha)
-      var lines = txtData.split('\n');
-      var tableBody = document.querySelector("#dados_sanavita_essencial tbody");
-
-      // Itera sobre cada linha
-      lines.forEach(function(line) {
-        // Divide a linha em colunas com base na tabulação
-        var columns = line.split('\t');
-
-        // Cria uma nova linha na tabela
-        var row = document.createElement('tr');
-
-        // Adiciona cada coluna à linha
-        columns.forEach(function(column) {
-          var cell = document.createElement('td');
-          cell.textContent = column;
-          row.appendChild(cell);
-        });
-
-        // Adiciona a linha ao corpo da tabela
-        tableBody.appendChild(row);
-      });
-    }
-  };
-
-  xhr.send();
-});
-
-// TABELA SANAVITA VITAL
-document.addEventListener("DOMContentLoaded", function() {
-  // URL do arquivo txt
-  var txtFile = 'dados/dados_sanavita_vital.txt';
-
-  // Cria um objeto XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', txtFile, true);
-
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      // Obtém o texto do arquivo
-      var txtData = xhr.responseText;
-
-      // Divide o texto em linhas (cada linha é separada por uma nova linha)
-      var lines = txtData.split('\n');
-      var tableBody = document.querySelector("#dados_sanavita_vital tbody");
-
-      // Itera sobre cada linha
-      lines.forEach(function(line) {
-        // Divide a linha em colunas com base na tabulação
-        var columns = line.split('\t');
-
-        // Cria uma nova linha na tabela
-        var row = document.createElement('tr');
-
-        // Adiciona cada coluna à linha
-        columns.forEach(function(column) {
-          var cell = document.createElement('td');
-          cell.textContent = column;
-          row.appendChild(cell);
-        });
-
-        // Adiciona a linha ao corpo da tabela
-        tableBody.appendChild(row);
-      });
-    }
-  };
-
-  xhr.send();
-});
-
 // TABELA LOREL DPGP OURO
 document.addEventListener("DOMContentLoaded", function() {
   // URL do arquivo txt
@@ -817,6 +506,90 @@ document.addEventListener("DOMContentLoaded", function() {
           index === lines.length - 1) { // Última linha
         row.classList.add('bold-data-row');
       }
+        
+        // Adiciona a linha ao corpo da tabela
+        tableBody.appendChild(row);
+      });
+    }
+  };
+
+  xhr.send();
+});
+
+// TABELA INTEGRAL MÉDICA FIT
+document.addEventListener("DOMContentLoaded", function() {
+  // URL do arquivo txt
+  var txtFile = 'dados/dados_integralmedica_fit.txt';
+
+  // Cria um objeto XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', txtFile, true);
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Obtém o texto do arquivo
+      var txtData = xhr.responseText;
+
+      // Divide o texto em linhas (cada linha é separada por uma nova linha)
+      var lines = txtData.split('\n');
+      var tableBody = document.querySelector("#dados_integralmedica_fit tbody");
+
+      // Itera sobre cada linha
+      lines.forEach(function(line, index) {
+        // Divide a linha em colunas com base na tabulação
+        var columns = line.split('\t');
+
+        // Cria uma nova linha na tabela
+        var row = document.createElement('tr');
+
+        // Adiciona cada coluna à linha
+        columns.forEach(function(column) {
+          var cell = document.createElement('td');
+          cell.textContent = column;
+          row.appendChild(cell);
+        });
+        
+        // Adiciona a linha ao corpo da tabela
+        tableBody.appendChild(row);
+      });
+    }
+  };
+
+  xhr.send();
+});
+
+// TABELA INTEGRAL MÉDICA PLUS
+document.addEventListener("DOMContentLoaded", function() {
+  // URL do arquivo txt
+  var txtFile = 'dados/dados_integralmedica_plus.txt';
+
+  // Cria um objeto XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', txtFile, true);
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Obtém o texto do arquivo
+      var txtData = xhr.responseText;
+
+      // Divide o texto em linhas (cada linha é separada por uma nova linha)
+      var lines = txtData.split('\n');
+      var tableBody = document.querySelector("#dados_integralmedica_plus tbody");
+
+      // Itera sobre cada linha
+      lines.forEach(function(line, index) {
+        // Divide a linha em colunas com base na tabulação
+        var columns = line.split('\t');
+
+        // Cria uma nova linha na tabela
+        var row = document.createElement('tr');
+
+        // Adiciona cada coluna à linha
+        columns.forEach(function(column) {
+          var cell = document.createElement('td');
+          cell.textContent = column;
+          row.appendChild(cell);
+        });
         
         // Adiciona a linha ao corpo da tabela
         tableBody.appendChild(row);
