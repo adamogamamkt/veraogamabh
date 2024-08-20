0f5ca66d5fa980a784b414c1abf76a3e8f83fde4
adamogamamkt <adamo.alighieri@distribuidoragama.com.br>
2024-08-20 10:00:06 -0300

    SC Johnson

--- js/scripts.js
+++ js/scripts.js
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function() {
           index === lines.length - 2 || // Penúltima linha
           index === lines.length - 1) { // Última linha
         row.classList.add('bold-data-row');
-      }
+        }
         
         // Adiciona a linha ao corpo da tabela
         tableBody.appendChild(row);
@@ -884,6 +884,114 @@ document.addEventListener("DOMContentLoaded", function() {
           cell.textContent = column;
           row.appendChild(cell);
         });
+        
+        // Adiciona a linha ao corpo da tabela
+        tableBody.appendChild(row);
+      });
+    }
+  };
+
+  xhr.send();
+});
+
+// TABELA SC JOHNSON OURO
+document.addEventListener("DOMContentLoaded", function() {
+  // URL do arquivo txt
+  var txtFile = 'dados/dados_scjohnson_ouro.txt';
+
+  // Cria um objeto XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', txtFile, true);
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Obtém o texto do arquivo
+      var txtData = xhr.responseText;
+
+      // Divide o texto em linhas (cada linha é separada por uma nova linha)
+      var lines = txtData.split('\n');
+      var tableBody = document.querySelector("#dados_scjohnson_ouro tbody");
+
+      // Itera sobre cada linha
+      lines.forEach(function(line, index) {
+        // Divide a linha em colunas com base na tabulação
+        var columns = line.split('\t');
+
+        // Cria uma nova linha na tabela
+        var row = document.createElement('tr');
+
+        // Adiciona cada coluna à linha
+        columns.forEach(function(column) {
+          var cell = document.createElement('td');
+          cell.textContent = column;
+          row.appendChild(cell);
+        });
+
+        // Adiciona a classe 'bold-data-row' às linhas desejadas
+        if (index === 0 ||              // Primeira linha
+          index === 1 ||              // Segunda linha
+          index === 2 ||              // Terceira linha
+          index === 3 ||              // Quarta linha
+          index === lines.length - 4 || // Antepenúltima linha
+          index === lines.length - 3 || // Antepenúltima linha
+          index === lines.length - 2 || // Penúltima linha
+          index === lines.length - 1) { // Última linha
+        row.classList.add('bold-data-row');
+        }
+        
+        // Adiciona a linha ao corpo da tabela
+        tableBody.appendChild(row);
+      });
+    }
+  };
+
+  xhr.send();
+});
+
+// TABELA SC JOHNSON PRATA
+document.addEventListener("DOMContentLoaded", function() {
+  // URL do arquivo txt
+  var txtFile = 'dados/dados_scjohnson_prata.txt';
+
+  // Cria um objeto XMLHttpRequest
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', txtFile, true);
+
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Obtém o texto do arquivo
+      var txtData = xhr.responseText;
+
+      // Divide o texto em linhas (cada linha é separada por uma nova linha)
+      var lines = txtData.split('\n');
+      var tableBody = document.querySelector("#dados_scjohnson_prata tbody");
+
+      // Itera sobre cada linha
+      lines.forEach(function(line, index) {
+        // Divide a linha em colunas com base na tabulação
+        var columns = line.split('\t');
+
+        // Cria uma nova linha na tabela
+        var row = document.createElement('tr');
+
+        // Adiciona cada coluna à linha
+        columns.forEach(function(column) {
+          var cell = document.createElement('td');
+          cell.textContent = column;
+          row.appendChild(cell);
+        });
+
+        // Adiciona a classe 'bold-data-row' às linhas desejadas
+        if (index === 0 ||              // Primeira linha
+          index === 1 ||              // Segunda linha
+          index === 2 ||              // Terceira linha
+          index === 3 ||              // Quarta linha
+          index === 4 ||              // Quinta linha
+          index === 5 ||              // Sexta linha
+          index === 6 ||              // Sétima linha
+          index === lines.length - 1) { // Última linha
+        row.classList.add('bold-data-row');
+        }
         
         // Adiciona a linha ao corpo da tabela
         tableBody.appendChild(row);
